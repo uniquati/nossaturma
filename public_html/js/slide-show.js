2c9a24d258619e8a70f191d1c7983213a1356400
Gilmar Andrade <gilmar.victhur@hotmail.com>
2015-07-01 23:53:18 -0300

    o slide pode ser aberto clicando em um elmento da classe slide-show

--- public_html/js/slide-show.js
+++ public_html/js/slide-show.js
@@ -4,6 +4,13 @@ Javascript do SLIDE SHOW
 function toggleLegenda(){
     $('#slideshow .slideshow-legenda').slideToggle('fast');
 }
+function abrirSlideShow(){
+    $('#slideshow .slideshow-legenda div').html( $(this).attr('data-legenda') );
+    $('#slideshow .slideshow-imagem').attr('src', $(this).attr('data-imagem') );
+    $('body').addClass('no-scroll');
+    $('#slideshow').fadeIn('slow');
+    $('#slideshow').addClass('aberto');
+}
 function fecharSlideShow(){
     $('body').removeClass('no-scroll');
     $('#slideshow').fadeOut('slow');
@@ -20,10 +27,10 @@ function resizeSlideShow(){
 }
 function initSlideShow(){
     console.log('INIT SLIDESHOW');
-   // $('body').append("<div id='slideshow' class='aberto'><div class='slideshow-cortina' onclick='toggleLegenda();'><img class='pure-img'  src='media/imagem1.jpg' /><div class='slideshow-legenda'>Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  ut labore et dolore magna aliqua. Ut enim ad minim veniam, ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  ut labore et dolore magna aliqua. Ut enim ad minim veniam, ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div><button onclick='fecharSlideShow();' class='fechar'></button><div class='prev'></div><div class='next'></div></div>");
-    
-    $('body').append("<div id='slideshow' class='aberto'><div class='slideshow-cortina' onclick='toggleLegenda();'><img class='slideshow-imagem pure-img'  src='media/_MG_7925essa[web].jpg' /><div class='slideshow-legenda'><div>Lorem ipsum dolor sit amet, consectetur adipisicing.</div></div></div><button onclick='fecharSlideShow();' class='fechar'></button><div class='prev'></div><div class='next'></div></div>");
+    $('body').append("<div id='slideshow' class='aberto'><div class='slideshow-cortina' onclick='toggleLegenda();'><img class='slideshow-imagem pure-img'  src='' /><div class='slideshow-legenda'><div></div></div></div><button onclick='fecharSlideShow();' class='fechar'></button><div class='prev'></div><div class='next'></div></div>");
     //$('body').append("<div id='slideshow' class='aberto'><div class='slideshow-cortina' onclick='toggleLegenda();'><img class='pure-img'  src='media/imagem1.jpg' /><div class='slideshow-legenda'><div>Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  ut labore et dolore magna aliqua. Ut enim ad minim veniam, ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  ut labore et dolore magna aliqua. Ut enim ad minim veniam, ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div></div><button onclick='fecharSlideShow();' class='fechar'></button><div class='prev'></div><div class='next'></div></div>");
     
     resizeSlideShow();
+    
+    $('.slide-show').click(abrirSlideShow);
 }
