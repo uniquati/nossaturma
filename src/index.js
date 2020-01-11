d09a4427fa92d507f425ffa73095f08cd276fb25
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-01-11 19:55:00 -0300

    correção window.onload

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const countdown = new Countdown(
 countdown.init();
 
 const diasJuntos = new Ciklo({ startDate: '08 March 2010 00:00:00 GMT-0300', endDate: '08 March 2020 00:00:00 GMT-0300' });
-alert(diasJuntos.elapsed.days);
-document.querySelector('#elapsedDays').innerHTML = ''+diasJuntos.elapsed.days;
 
+window.onload = function() {
+  document.querySelector('#elapsedDays').innerHTML = ''+diasJuntos.elapsed.days;
+};
