7112d1559769157a38910c32b57de3049739e3b6
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-02-03 10:08:56 -0300

    atração entre particulas

--- src/Particles.js
+++ src/Particles.js
@@ -1,3 +1,7 @@
+/**
+ * inspirado no Particle.js
+ * https://vincentgarreau.com/particles.js
+ */
 import Particle from './Particle';
 
 export default class Particles {
@@ -22,10 +26,11 @@ export default class Particles {
     }
 
     draw(){
-        this.ctx.clearRect(0, 0, 800, 600); // clear canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
 
-        this.particlesArray.forEach(particle => {
+        this.particlesArray.forEach((particle, i) => {
             // console.log(particle.direction.x * particle.speed/100);
+            /* move particle */
             particle.x += particle.direction.x * particle.speed/100;
             particle.y += particle.direction.y * particle.speed/100;
 
@@ -35,6 +40,10 @@ export default class Particles {
             if(particle.y > this.canvas.height + particle.radius*4) particle.y = -particle.radius*2;
             else if(particle.y < 0 - particle.radius*4) particle.y = this.canvas.height + particle.radius*2;
 
+            /* interactions between particles */
+            for(let j = i + 1; j < this.particlesArray.length; j++) {
+                this.attractParticles(particle, this.particlesArray[j]);
+            }
             this.ctx.fillStyle = particle.color;
             this.ctx.beginPath();
             this.ctx.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2, false);
@@ -49,5 +58,30 @@ export default class Particles {
         this.ctx.closePath();
         this.ctx.stroke();
         window.requestAnimationFrame(this.draw.bind(this));
+    }
+
+    /**
+     * Atração das particulas
+     * @param {*} p1 
+     * @param {*} p2 
+     */
+    attractParticles(p1, p2) {
+        /* condensed particles */
+        const dx = p1.x - p2.x,
+        dy = p1.y - p2.y,
+        dist = Math.sqrt(dx*dx + dy*dy);
+
+        if(dist <= 100){
+            // console.log(dist);
+            const ax = dx/(3*1000),
+            ay = dy/(3*1000);
+            
+            p1.x -= ax;
+            p1.y -= ay;
+
+            p2.x += ax;
+            p2.y += ay;
+
+        }
     }
 }
